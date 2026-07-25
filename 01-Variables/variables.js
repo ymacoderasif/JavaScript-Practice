65a6a9dbe34fe1b84453f8654dbe4d8b0b876d3f
ymacoderasif <mohdasif8x@gmail.com>
2026-07-25 23:07:26 +0530

    variable q7  solved

--- 01-Variables/variables.js
+++ 01-Variables/variables.js
@@ -1,6 +1,10 @@
-/*Q1. Store Your Name 
+/*
+=================================================================
+Q1. Store Your Name 
 Question: 
-Ek variable banao aur usme apna naam store karke print karo. */
+Ek variable banao aur usme apna naam store karke print karo. 
+=================================================================
+*/
 
 let Name = "Mohd Asif";
 console.log(Name);
@@ -11,9 +15,13 @@ console.log(Name);
 
 
 
-/*Q2. Store Your Age 
+/*
+=====================================================
+Q2. Store Your Age 
 Question: 
-Ek variable me apni age store karke print karo.*/
+Ek variable me apni age store karke print karo.
+=====================================================
+*/
 
 let age = 24;
 console.log(age);
@@ -22,9 +30,12 @@ console.log(age);
 
 
 
-/*Q3. Store City Name 
+/*=========================================================
+Q3. Store City Name 
 Question: 
-Ek variable me apne city ka naam store karke print karo.*/
+Ek variable me apne city ka naam store karke print karo.
+===========================================================
+*/
 
 let city = "Noida";
 console.log(city);
@@ -33,9 +44,12 @@ console.log(city);
 
 
 
-/*Q4. Store Mobile Number 
+/*=====================================================
+Q4. Store Mobile Number 
 Question: 
-Ek variable me mobile number store karke print karo.*/
+Ek variable me mobile number store karke print karo.
+=======================================================
+*/
 
 let number = 8376845742;
 console.log(number);
@@ -44,9 +58,12 @@ console.log(number);
 
 
 
-/*Q5. Store Favorite Color 
+/*=============================================================
+Q5. Store Favorite Color 
 Question: 
-Ek variable me apna favorite color store karke print karo.*/
+Ek variable me apna favorite color store karke print karo.
+===============================================================
+*/
 
 let color = "Black";
 console.log(color);
@@ -55,10 +72,27 @@ console.log(color);
 
 
 
-/*Q6. Store Two Numbers 
+/*===============================================================
+Q6. Store Two Numbers 
 Question: 
-Do alag variables me do numbers store karo aur dono print karo.*/
+Do alag variables me do numbers store karo aur dono print karo.
+=================================================================
+*/
 
 let firstnum = 10;
 let secondnum = 20;
 console.log(firstnum,secondnum);
+
+
+
+
+/*=================================================== 
+Q7. Store Boolean Value 
+Question: 
+Ek variable me true ya false store karke print karo.
+=====================================================
+*/
+
+
+let booleanval = true;
+console.log(booleanval);
