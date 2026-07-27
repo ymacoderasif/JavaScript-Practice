a53e373922d239d4ab353921c501a034bae8b2cb
ymacoderasif <mohdasif8x@gmail.com>
2026-07-27 20:41:34 +0530

    if else q2, q3 solved

--- 02-If-Else/if-else.js
+++ 02-If-Else/if-else.js
@@ -1,4 +1,4 @@
-/*                                              ============== All The Best ===================                                                  /*
+/*                                                                              ============== All The Best ===================                                                  /*
 
 
 
@@ -30,3 +30,85 @@ if(userAge >= 18 && userBudget >= ticketPrice){
 }
 
 
+
+
+
+
+
+/*
+=========================================================================
+Q2. ATM Cash Withdrawal 
+Question 
+Ek ATM System banao. 
+• Account Holder ka naam store karo.  
+• Account Balance store karo.  
+• Withdraw Amount store karo.  
+• Agar withdrawal possible hai to transaction complete karo.  
+• Transaction ke baad updated balance bhi dikhana hai.  
+• Agar withdrawal possible nahi hai to proper message dikhana 
+hai.  
+=========================================================================
+*/
+
+let accountHolder = "Mohd Asif";
+let accountBalance = 50000;
+let withdrawAmount = Number(prompt("Enter a Withdrawal Amount:"));
+let updatedBalance = accountBalance - withdrawAmount;
+if(accountBalance >= withdrawAmount){
+    console.log(`Dear ${accountHolder}, Your Transaction is Successful`);
+    console.log(`Updated Balance: ₹${updatedBalance}`);
+}else{
+    console.log("❌ Transaction Failed");
+    console.log("Reason: Insufficient Balance");
+}
+
+
+
+
+
+
+
+
+/*
+=============================================================
+Q3. Student Result System 
+Question 
+Ek Student Result System banao. 
+• Student ka naam store karo.  
+• Total Marks store karo.  
+• Passing Marks store karo.  
+• Agar student pass hota hai to result dikhana hai.  
+• Warna fail dikhana hai.  
+• Student ka naam aur result bhi dikhana hai.  
+=============================================================
+*/
+
+let studentName = "Rahul";
+let studentTotalMarks = 65;
+let passingMarks = 33;
+if(studentTotalMarks >= passingMarks){
+    console.log(`Congratulations ${studentName}! You Have Passed. Marks: ${studentTotalMarks}`);
+}else{
+    console.log(`Dear ${studentName}, You Have Failed. Marks: ${studentTotalMarks}`);   
+}
+
+
+
+
+
+
+
+/*
+===================================================================
+Q4. Shopping Discount System 
+Question 
+Ek Shopping Billing System banao. 
+• Customer ka naam store karo.  
+• Shopping Amount store karo.  
+• Discount Amount store karo.  
+• Agar customer discount ke liye eligible hai to discount 
+calculate karo.  
+• Final payable amount nikalo.  
+• Customer ka naam, discount aur final amount dikhana hai. 
+===================================================================
+*/
