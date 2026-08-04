719167a16e7280ce6ccef8d05f17c07aeadaa3ba
ymacoderasif <mohdasif8x@gmail.com>
2026-08-04 12:23:12 +0530

    Loop Q14 Solved

--- 03-Loops/loop.js
+++ 03-Loops/loop.js
@@ -241,3 +241,25 @@ for(let i = 1; i <= 50; i++){
     sum = i + sum;
 }
 console.log(sum);
+
+
+
+
+
+
+
+
+/*
+=============================================================
+Q14.Find the sum of all even numbers from 1 to 20. 
+=============================================================
+*/
+let sum = 0;
+for(let i = 1; i <= 20; i++){
+    if(i%2===0){
+        sum = i + sum;
+    }
+}
+console.log(sum);
+
+
