77bdc0ae71824a651977d8861dbce5f63072ea12
ymacoderasif <mohdasif8x@gmail.com>
2026-08-15 17:31:16 +0530

    Function Q1 Solved

---
+++ 04-Functions/functions.js
@@ -0,0 +1,21 @@
+/*                                                                              ============== All The Best ===================                                                  */
+
+
+
+
+
+
+
+
+/*
+========================================================
+Q1.  
+Ek function banao jo console me print kare:  
+Hello JavaScript  
+Function ko call bhi karo. 
+========================================================
+*/
+function print(){
+    console.log("Hello JavaScript ");   
+}
+print();
