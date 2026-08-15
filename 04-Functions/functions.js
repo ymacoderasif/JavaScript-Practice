f0c3e3791ee41488769f19252435ecd589db4fe0
ymacoderasif <mohdasif8x@gmail.com>
2026-08-15 20:54:29 +0530

    Function Q12 Solved

--- 04-Functions/functions.js
+++ 04-Functions/functions.js
@@ -284,3 +284,35 @@ function checkLeapYear(leapYear){
     }
 }
 checkLeapYear(2000)
+
+
+
+
+
+
+
+
+/*
+=================================================================
+Q12.  
+Ek function banao jo FizzBuzz print kare 1 se n tak.  
+Example: fizzBuzz(20); 
+=================================================================
+*/
+function fizzBuzz(fizzBuzzLimit){
+    for(let i = 1;i <= fizzBuzzLimit;i++){
+        if(i % 3 === 0 && i % 5 === 0){
+            console.log("FizzBuzz");
+        }else if(i % 3 === 0){
+            console.log("Fizz");
+        }else if(i % 5 === 0){
+            console.log("Buzz");
+        }
+        else{
+            console.log(i);
+            
+        }
+
+    }
+}
+fizzBuzz(20)
